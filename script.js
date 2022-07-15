67281d9d1c6174f8026e45a9ee103b2dd1c20379
Dev Patidar <63471580+develop084@users.noreply.github.com>
2022-07-15 09:33:10 +0000

    Some modification in script.js and also added some event

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
-const accessKey = 'wgPFD6y6RQH9tvGksuj9F6hHZCxLWxC7FkRhychBaBU'
+const imgContainer = document.getElementById("img-container");
+const loader = document.getElementById("loader");
+
+const accessKey = "wgPFD6y6RQH9tvGksuj9F6hHZCxLWxC7FkRhychBaBU";
 const Numcount = 30;
 const API = `https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${Numcount}`;
 
+// get photos from api function
 
+let photosArray = [];
 
-// get photos from api function 
-
-async function getPhotos(){
-  try{
-    const response = await fetch(API); 
-    const data = await response.json();
-    console.log(data)
-  }
-  catch(error){
+async function getPhotos() {
+  try {
+    const response = await fetch(API);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
     // catch error
     console.log(error);
   }
 }
 
+// check if we arrived at the bottom of page
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    console.log("load more!!");
+    getPhotos();
+  }
+});
 
-// ON LOAD 
+//display photo function (links and photos)
+function displayPhotos() {
+  // forEach over the array
+  photosArray.forEach((photo) => {
+    console.log(photo.links.html);
+    // creating <a> element to link the image
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+
+    // create image tag
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.description);
+    img.setAttribute("title", photo.description);
+
+    // put <img> tag inside <a> tag
+
+    item.appendChild(img);
+    imgContainer.appendChild(item);
+  });
+}
+
+// ON LOAD
 getPhotos();
